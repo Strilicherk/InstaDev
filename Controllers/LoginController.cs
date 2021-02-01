2e8fc5630e717d95ce1b91bf9584366ceec3b2a0
FIC <FIC@info.local>
2021-02-01 16:50:22 -0300

    Metade do back feito

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,7 +1,51 @@
+using System.Collections.Generic;
+using InstaDev.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
 namespace InstaDev.Controllers
 {
-    public class LoginController
+    public class LoginController : Controller
     {
-        
-    }
+
+        public string Mensagem { get; set; }
+        User user = new User();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Logar(IFormCollection form)
+        {
+            // Lista todas as linhas do CSV ("banco de dados")
+            List<string> csv = user.ReadAllLinesCSV("Database/User.csv");
+
+            // Verifica se o usuário já estiver cadastrado ou logado uma conta
+            // ele vai buscar lá no csv
+            var logado = 
+            csv.Find(
+                x => 
+                x.Split(";")[0] == form["Email"] && 
+                x.Split(";")[3] == form["Password"]
+            );
+
+
+            // se encontrar um usuário, ele irá redirecionar para a página de feed
+             if(logado != null)
+            {
+                HttpContext.Session.SetString("_UserName", logado.Split(";")[0]);
+                return LocalRedirect("Feed");
+            }
+
+             Mensagem = "Senha ou email incorretos, tente novamente...";
+            return LocalRedirect("~/Login");
+            }
+
+            [Microsoft.AspNetCore.Mvc.Route("Logout")]
+            public IActionResult Logout()
+            {
+                HttpContext.Session.Remove("_UserName");
+                return LocalRedirect("~/");
+            }
+            }
 }
