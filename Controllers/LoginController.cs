03a74dccb783efbc893948c505d271a3783eee20
FIC <FIC@info.local>
2021-02-02 16:26:45 -0300

    Fazendo alterações

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -7,7 +7,7 @@ namespace InstaDev.Controllers
 {
     public class LoginController : Controller
     {
-        
+
         [TempData]
         public string Mensagem { get; set; }
         User user = new User();
@@ -23,30 +23,30 @@ namespace InstaDev.Controllers
 
             // Verifica se o usuário já estiver cadastrado ou logado uma conta
             // ele vai buscar lá no csv
-            var logado = 
+            var logado =
             csv.Find(
-                x => 
-                x.Split(";")[0] == form["Email"] && 
+                x =>
+                x.Split(";")[0] == form["Email"] &&
                 x.Split(";")[3] == form["Password"]
             );
 
 
             // se encontrar um usuário, ele irá redirecionar para a página de feed
-             if(logado != null)
+            if (logado != null)
             {
                 HttpContext.Session.SetString("_UserName", logado.Split(";")[0]);
-                return LocalRedirect("Feed");
+                return LocalRedirect("~/Feed");
             }
 
-             Mensagem = "Senha ou email incorretos, tente novamente...";
+            Mensagem = "Senha ou email incorretos, tente novamente...";
             return LocalRedirect("~/Login");
-            }
+        }
 
-            [Microsoft.AspNetCore.Mvc.Route("Logout")]
-            public IActionResult Logout()
-            {
-                HttpContext.Session.Remove("_UserName");
-                return LocalRedirect("~/");
-            }
-            }
+        [Route("Logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("_UserName");
+            return LocalRedirect("~/");
+        }
+    }
 }
