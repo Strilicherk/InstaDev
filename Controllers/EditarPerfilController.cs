52e3d91119d30c9c380ac99c3dfbd4be323e4e80
MatheusStrilicherkDEV <matheuscherk@hotmail.com>
2021-02-04 08:35:46 -0300

    .

--- Controllers/EditarPerfilController.cs
+++ Controllers/EditarPerfilController.cs
@@ -5,20 +5,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace InstaDev.Controllers
 {
+    [Route("EditarPerfil")]
     public class EdicaoPerfilController : Controller
     {
         User userModel = new User();
-        [Route("EditarPerfil")]
+        // [Route("Edi")]
         public IActionResult Index()
         {
             // ViewBag.DadosUsuario = userModel.Read
             return View();
         }
-        public IActionResult Excluir(int id)
-        {
-            userModel.Delete(id);
-            return LocalRedirect ("~/Register");
-        }
+        // public IActionResult Excluir(int id)
+        // {
+        //     userModel.Delete(id);
+        //     return LocalRedirect ("~/Register");
+        // }
         // public IActionResult Update(User user)
         // {
         //     List<string> linhas = ReadAllLinesCSV (PATH); PATH csv que o cadastro armazena infos
